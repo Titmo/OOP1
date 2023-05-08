28b6d5552777d12a7fc9519e9bee85ada190fc96
Danila <03denchik03@gmail.com>
2023-05-08 21:16:24 +0300

    сделал

---
+++ src/Hogwarts.java
@@ -0,0 +1,25 @@
+public class Hogwarts {
+    private String name;
+    private int powerMagic;
+    private int distanceTransgression;
+
+
+    public Hogwarts(String name, int powerMagic, int distanceTransgression) {
+        this.name = name;
+        this.powerMagic = powerMagic;
+        this.distanceTransgression = distanceTransgression;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPowerMagic() {
+        return powerMagic;
+    }
+
+    public int getDistanceTransgression() {
+        return distanceTransgression;
+    }
+
+}
---
+++ src/Hufflepuff.java
@@ -0,0 +1,36 @@
+public class Hufflepuff extends Hogwarts{
+    private int hardworking;
+    private int loyal;
+    private int honest;
+
+    public Hufflepuff(String name, int powerMagic, int distanceTransgression, int hardworking, int loyal, int honest) {
+        super(name, powerMagic, distanceTransgression);
+        this.hardworking = hardworking;
+        this.loyal = loyal;
+        this.honest = honest;
+    }
+
+    public int getHardworking() {
+        return hardworking;
+    }
+
+    public void setHardworking(int hardworking) {
+        this.hardworking = hardworking;
+    }
+
+    public int getLoyal() {
+        return loyal;
+    }
+
+    public void setLoyal(int loyal) {
+        this.loyal = loyal;
+    }
+
+    public int getHonest() {
+        return honest;
+    }
+
+    public void setHonest(int honest) {
+        this.honest = honest;
+    }
+}
